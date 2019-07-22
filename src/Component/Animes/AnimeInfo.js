1414f36b1140df3d228e8354a15f1a5a48697281
Weslley Braga <wesbragagt@gmail.com>
2019-07-22 03:57:40 -0500

    rendering image

--- src/Component/Animes/AnimeInfo.js
+++ src/Component/Animes/AnimeInfo.js
@@ -2,8 +2,16 @@ import React, { Component, Fragment } from 'react';
 import Spinner from './Spinner';
 import PropTypes from 'prop-types';
 class AnimeInfo extends Component {
+    state = {
+        animeImg: ''
+    }
+    componentDidUpdate(){
+        console.log(this.state);
+    }
     componentDidMount() {
-        this.props.getAnime(this.props.match.params.id);
+        this.props.getAnime(this.props.match.params.id)
+            // had to set the api object img to state in order to render it after the call is finished
+            .then(()=>this.setState({animeImg: this.props.anime.posterImage}));
     }
 
     render() {
@@ -14,6 +22,7 @@ class AnimeInfo extends Component {
 
         return (
             <div>
+                <img style={{width: '17rem'}} src={this.state.animeImg.large} alt=""/>
                 <ul>
                     <li>{this.props.anime.canonicalTitle}</li>
                     <li>Rating: {this.props.anime.averageRating}</li>
